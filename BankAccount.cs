f866b54b6448bf9a5d32f0f8d21071ac61d67242
umme <unnipa01@yahoo.com>
2021-08-08 23:56:00 -0500

    initial copy

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    class BankAccount
+    {
+        
+            private double balance = 0;
+
+            public void Deposit(double deposit)
+            {
+                balance = balance + deposit;
+            }
+
+            public double GetBalance(double withdrawAmt)
+            {
+                return balance -= withdrawAmt;
+            }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    
+    
+
+    class Program
+    {
+             
+        static void Main(string[] args)
+        {
+            BankAccount cusBankBalance = new BankAccount();
+            
+            Console.WriteLine("Please enter the deposit amount ");
+            var amount = double.Parse(Console.ReadLine());         
+            cusBankBalance.Deposit(amount);
+            Console.WriteLine("Please enter the amount the money you would like withdraw");
+            var withdrawAmt = double.Parse(Console.ReadLine());
+
+            withdrawAmt = cusBankBalance.GetBalance(withdrawAmt);
+            Console.WriteLine($"your current balance is {withdrawAmt}");
+
+        }
+    }
+}
